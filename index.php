d58833fc529273289774d402a596033f47658b22
Timothy Piggott <piggott291@gmail.com>
2021-08-09 12:16:40 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+
+    <body>
+        None php data        
+        <?php
+        echo "Hello from tim";
+        exit
+        ?>
+    </body>
+</html>
+
+Something went wrong!
